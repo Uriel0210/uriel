95bd9a4451b03ee47a68bad252bdddc4fdd5ec34
uriel martinez <uriel123mar123@gmail.com>
2024-02-06 13:51:25 -0600

    primeros cambios al proyecto

---
+++ src/js/script1.js
@@ -0,0 +1,6 @@
+const location      =      "NewYork,"    +    "NY";
+const location2="NewYork,"+"NY";
+const location3= "NewYork,"       +"NY";
+console.log(location);
+console.log(location2);
+console.log(location3);
