84e61aa8533540662c75ff94acb81395a338a789
uriel martinez <uriel123mar123@gmail.com>
2024-02-13 13:13:06 -0600

    segundo cambio

--- src/js/script1.js
+++ src/js/script1.js
@@ -1,6 +1,17 @@
-const location      =      "NewYork,"    +    "NY";
-const location2="NewYork,"+"NY";
-const location3= "NewYork,"       +"NY";
-console.log(location);
-console.log(location2);
-console.log(location3);
+//para pedir datos
+/*let nombre1= prompt("primer nombre");
+let nombre2 = promt("segundo nombre");
+
+console.log(nombre1);
+console.log(nombre2);*/
+
+let edad = 20;
+let base = 4.5;
+
+
+
+
+
+
+
+  
